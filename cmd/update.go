a39145badf044574b7f2969ca0317942ee4b7920
Mystery0 <mystery0dyl520@gmail.com>
2020-05-24 14:08:32 +0800

    fix: 修复更新工具

--- cmd/update.go
+++ cmd/update.go
@@ -7,17 +7,18 @@ import (
 	"github.com/spf13/cobra"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
 const user = "Mystery00"
 const project = "KubectlPlugin"
-const fileName = "k8s-mac.zip"
-const k8sFileName = "k8s"
 const installPath = "/usr/local/bin/k8s"
+const tempDir = "/tmp/k8s/"
 const apiUrl = "https://api.github.com/repos/" + user + "/" + project + "/releases/latest"
-const downloadUrl = "https://github.com/" + user + "/" + project + "/releases/download/%s/" + fileName
+const downloadUrl = "https://github.com/" + user + "/" + project + "/releases/download/%s/"
 
 func init() {
 	rootCmd.AddCommand(updateCmd)
@@ -27,14 +28,30 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "更新工具版本。",
 	Run: func(cmd *cobra.Command, args []string) {
+		var fileName string
+		var k8sFileName string
+		switch runtime.GOOS {
+		case "darwin":
+			fileName = "k8s-mac.zip"
+			k8sFileName = "k8s-mac"
+		case "windows":
+			fmt.Println(utils.ERROR + " 暂不支持 Windows")
+			os.Exit(0)
+		case "linux":
+			fileName = "k8s-linux.zip"
+			k8sFileName = "k8s-linux"
+		default:
+			fmt.Println(utils.ERROR + " 未知操作系统")
+			os.Exit(0)
+		}
 		fmt.Println(utils.INFO + " 正在检查更新...")
 		cmdStr := `wget -qO- -t1 -T2 "` + apiUrl + `" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
 		latestVersion := strings.TrimSpace(utils.Cmd("sh", "-c", cmdStr))
 		if latestVersion != version {
 			//版本不一致，更新版本
 			fmt.Println(utils.INFO + " 检测到新版本 [" + latestVersion + "] ，正在下载...")
-			utils.DownloadFile(fmt.Sprintf(downloadUrl, latestVersion), fileName)
-			copyFile()
+			utils.DownloadFile(fmt.Sprintf(downloadUrl+fileName, latestVersion), fileName)
+			copyFile(fileName, k8sFileName)
 			fmt.Println("工具已更新为最新版本 [" + utils.Red(latestVersion) + "] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)")
 		} else {
 			fmt.Println(utils.WARN + " 当前已经是最新版本！")
@@ -42,16 +59,21 @@ var updateCmd = &cobra.Command{
 	},
 }
 
-func copyFile() {
-	err := unzip(fileName, ".")
+func copyFile(fileName string, k8sFileName string) {
+	err := unzip(fileName, tempDir)
 	if err != nil {
 		panic(err)
 	}
-	err = os.Rename(k8sFileName, installPath)
+	file, err := exec.LookPath(os.Args[0])
 	if err != nil {
 		panic(err)
 	}
-	err = os.RemoveAll(fileName)
+	path, err := filepath.Abs(file)
+	if err != nil {
+		panic(err)
+	}
+	currentFileName := filepath.Base(path)
+	err = os.Rename(tempDir+k8sFileName, currentFileName)
 	if err != nil {
 		panic(err)
 	}
